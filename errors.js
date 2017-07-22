3c41895f6726ee3879266f6b7f93d959ca27cda4
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-22 19:47:58 +0200

    rewrite errors to ES6 classes

--- errors.js
+++ errors.js
@@ -1,42 +1,43 @@
+class ModbusCrcError extends Error {
+    constructor() {
+        super();
+
+        this.message = 'Received Modbus response get invalid CRC';
+        this.name = 'ModbusCrcError';
+        Error.captureStackTrace(this, ModbusCrcError);
+    }
+}
+
+class ModbusAborted extends Error {
+    constructor() {
+        super();
+        this.message = 'Aborted';
+        this.name = 'ModbusAborted';
+        Error.captureStackTrace(this, ModbusAborted);
+    }
+}
+
+class ModbusRetryLimitExceed extends Error {
+    constructor(add) {
+        super();
+        this.message = 'Retry limit exceed ' + add;
+        this.name = 'ModbusRetryLimitExceed';
+        Error.captureStackTrace(this, ModbusRetryLimitExceed);
+    }
+}
+
+class ModbusResponseTimeout extends Error {
+    constructor(time) {
+        super();
+        this.message = `Response timeout of ${time}ms exceed!`;
+        this.name = 'ModbusResponseTimeout';
+        Error.captureStackTrace(this, ModbusResponseTimeout);
+    }
+}
+
 module.exports = {
     ModbusCrcError,
     ModbusAborted,
     ModbusRetryLimitExceed,
     ModbusResponseTimeout,
 };
-
-function ModbusCrcError() {
-    this.message = 'Received Modbus response get invalid CRC';
-    this.name = 'ModbusCrcError';
-    Error.captureStackTrace(this, ModbusCrcError);
-}
-
-ModbusCrcError.prototype = Object.create(Error.prototype);
-ModbusCrcError.prototype.constructor = ModbusCrcError;
-
-function ModbusAborted() {
-    this.message = 'Aborted';
-    this.name = 'ModbusAborted';
-    Error.captureStackTrace(this, ModbusAborted);
-}
-
-ModbusAborted.prototype = Object.create(Error.prototype);
-ModbusAborted.prototype.constructor = ModbusAborted;
-
-function ModbusRetryLimitExceed(add) {
-    this.message = 'Retry limit exceed ' + add;
-    this.name = 'ModbusRetryLimitExceed';
-    Error.captureStackTrace(this, ModbusRetryLimitExceed);
-}
-
-ModbusRetryLimitExceed.prototype = Object.create(Error.prototype);
-ModbusRetryLimitExceed.prototype.constructor = ModbusRetryLimitExceed;
-
-function ModbusResponseTimeout(time) {
-    this.message = `Response timeout ${time}ms exceed!`;
-    this.name = 'ModbusResponseTimeout';
-    Error.captureStackTrace(this, ModbusResponseTimeout);
-}
-
-ModbusResponseTimeout.prototype = Object.create(Error.prototype);
-ModbusResponseTimeout.prototype.constructor = ModbusResponseTimeout;
