56cbe9733501c4d08ecb6326a652db17f3f13915
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-22 14:14:42 +0200

    update modbus master to ES6 class, clean up

--- errors.js
+++ errors.js
@@ -1,7 +1,7 @@
 module.exports = {
-    crc: ModbusCrcError,
+    ModbusCrcError,
     abort: ModbusAborted,
-    retryLimit: ModbusRetryLimitExceed,
+    ModbusRetryLimitExceed,
     ModbusResponseTimeout,
 };
 
--- master.js
+++ master.js
@@ -1,154 +1,165 @@
-var crc = require('crc');
-var BufferPut = require('bufferput');
-var Promise = require("bluebird");
-var SerialHelper = require('./serial-helper');
-var constants = require('./constants');
-var _ = require('lodash');
-var errors = require('./errors');
-var packetUtils = require('./packet-utils');
+const BufferPut = require('bufferput');
+const Promise = require('bluebird');
+const SerialHelper = require('./serial-helper');
+const {
+    FUNCTION_CODES,
+    RESPONSE_TIMEOUT,
+    QUEUE_TIMEOUT,
+    DEFAULT_RETRY_COUNT,
+} = require('./constants');
+const errors = require('./errors');
+const packetUtils = require('./packet-utils');
 
-module.exports = Master;
+module.exports.ModbusMaster = ModbusMaster;
 module.exports.DATA_TYPES = packetUtils.DATA_TYPES;
 
-function Master(serialPort, options) {
-    serialPort.on('error', function(err) {
-        console.error(err);
-    });
+class ModbusMaster {
+    constructor(serialPort, options) {
+        serialPort.on('error', (err) => {
+            console.error(err);
+        });
 
-    this._options =  _.defaults(options || {}, {
-      responseTimeout: constants.RESPONSE_TIMEOUT,
-      queueTimeout: constants.QUEUE_TIMEOUT,
-    });
+        this._options = Object.assign({}, {
+            responseTimeout: RESPONSE_TIMEOUT,
+            queueTimeout: QUEUE_TIMEOUT,
+        }, options || {});
 
-    this.serial = new SerialHelper(serialPort, this._options);
-}
+        this.serial = new SerialHelper(serialPort, this._options);
+    }
 
-/**
- * Modbus function read holding registers
- * @param {number} slave
- * @param {number} start
- * @param {number} length
- * @param {number | function?} dataType value from DATA_TYPES const or callback
- * @returns {Promise<number[]>}
- */
-Master.prototype.readHoldingRegisters = function(slave, start, length, dataType) {
-    var packet = this.createFixedPacket(slave, constants.FUNCTION_CODES.READ_HOLDING_REGISTERS, start, length);
+    /**
+     * Modbus function read holding registers
+     * @param {number} slave
+     * @param {number} start
+     * @param {number} length
+     * @param {number | function} [dataType] value from DATA_TYPES const or callback
+     * @returns {Promise<number[]>}
+     */
+    readHoldingRegisters(slave, start, length, dataType) {
+        const packet = this.createFixedPacket(slave, FUNCTION_CODES.READ_HOLDING_REGISTERS, start, length);
 
-    return this.request(packet).then(function(buffer) {
-        var buf = packetUtils.getDataBuffer(buffer);
+        return this.request(packet).then((buffer) => {
+            const buf = packetUtils.getDataBuffer(buffer);
 
-        if (_.isFunction(dataType)) {
-            return dataType(buf);
-        }
-
-        return packetUtils.parseFc03Packet(buf, dataType);
-    });
-};
-
-/**
- *
- * @param {number} slave
- * @param {number} register
- * @param {number} value
- * @param {number} retryCount
- */
-Master.prototype.writeSingleRegister = function (slave, register, value, retryCount) {
-    var packet = this.createFixedPacket(slave, constants.FUNCTION_CODES.WRITE_SINGLE_REGISTER, register, value);
-    var self = this;
-    retryCount = retryCount ? retryCount : constants.DEFAULT_RETRY_COUNT;
-
-    var performRequest = function (retry) {
-        return new Promise(function (resolve, reject) {
-            var funcName = 'writeSingleRegister: ';
-            var funcId  = ' Slave '+slave+'; ' +
-                'Register: '+register+'; Value: '+value+';  Retry ' + (retryCount + 1 - retry) + ' of ' + retryCount;
-
-            if (retry <= 0) {
-                throw new errors.retryLimit(funcId);
+            if (typeof (dataType) === 'function') {
+                return dataType(buf);
             }
 
-            self._options.debug &&
-            console.log(funcName + 'perform request.' + funcId);
-
-            self.request(packet)
-                .then(resolve)
-                .catch(function (err) {
-                    self._options.debug && console.log(funcName + err  + funcId);
-
-                    return performRequest(--retry)
-                        .then(resolve)
-                        .catch(reject);
-                });
+            return packetUtils.parseFc03Packet(buf, dataType);
         });
-    };
-    return performRequest(retryCount);
-};
+    }
 
-/**
- *
- * @param {number} slave
- * @param {number} start
- * @param {number[]} array
- */
-Master.prototype.writeMultipleRegisters = function(slave, start, array){
-    var packet = this.createVariousPacket(slave, constants.FUNCTION_CODES.WRITE_MULTIPLE_REGISTERS, start, array);
-    return this.request(packet);
-};
+    /**
+     *
+     * @param {number} slave
+     * @param {number} register
+     * @param {number} value
+     * @param {number} [retryCount]
+     */
+    writeSingleRegister(slave, register, value, retryCount) {
+        const packet = this.createFixedPacket(slave, FUNCTION_CODES.WRITE_SINGLE_REGISTER, register, value);
+        retryCount = retryCount || DEFAULT_RETRY_COUNT;
 
-/**
- * Create modbus packet with fixed length
- * @param slave
- * @param func
- * @param param
- * @param param2
- * @private
- * @returns {*}
- */
-Master.prototype.createFixedPacket = function(slave, func, param, param2){
-   return (new BufferPut())
-        .word8be(slave)
-        .word8be(func)
-        .word16be(param)
-        .word16be(param2)
-        .buffer();
-};
+        const performRequest = (retry) => {
+            return new Promise((resolve, reject) => {
+                const funcName = 'writeSingleRegister: ';
+                const funcId =
+                    `Slave ${slave}; Register: ${register}; Value: ${value};` +
+                    `Retry ${retryCount + 1 - retry} of ${retryCount}`;
 
-/**
- * Create modbus packet with various length
- * @param slave
- * @param func
- * @param start
- * @param array
- * @private
- */
-Master.prototype.createVariousPacket = function(slave, func, start, array){
-    var buf = (new BufferPut())
-        .word8be(slave)
-        .word8be(func)
-        .word16be(start)
-        .word16be(array.length)
-        .word8be(array.length*2);
+                if (retry <= 0) {
+                    throw new errors.ModbusRetryLimitExceed(funcId);
+                }
 
-    _.forEach(array, function(value){
-        buf.word16be(value);
-    });
+                this.log(funcName + 'perform request.' + funcId);
 
-    return buf.buffer();
-};
+                this.request(packet)
+                    .then(resolve)
+                    .catch((err) => {
+                        this.log(funcName + err + funcId);
 
-/**
- * @private
- * @param {Buffer} buffer
- * @returns {Promise<Buffer>}
- */
-Master.prototype.request = function request(buffer) {
-    var self = this;
+                        return performRequest(--retry)
+                            .then(resolve)
+                            .catch(reject);
+                    });
+            });
+        };
+        return performRequest(retryCount);
+    }
 
-    return this.serial.write(packetUtils.addCrc(buffer))
-        .then(function (response) {
-            if (!packetUtils.checkCrc(response))
-                throw new errors.crc;
-            return response;
-        })
-};
+    /**
+     *
+     * @param {number} slave
+     * @param {number} start
+     * @param {number[]} array
+     */
+    writeMultipleRegisters(slave, start, array) {
+        const packet = this.createVariousPacket(slave, FUNCTION_CODES.WRITE_MULTIPLE_REGISTERS, start, array);
+        return this.request(packet);
+    }
 
+    /**
+     * Create modbus packet with fixed length
+     * @private
+     * @param {number} slave
+     * @param {number} func
+     * @param {number} param
+     * @param {number} param2
+     * @returns {Buffer}
+     */
+    createFixedPacket(slave, func, param, param2) {
+        return (new BufferPut())
+            .word8be(slave)
+            .word8be(func)
+            .word16be(param)
+            .word16be(param2)
+            .buffer();
+    }
+
+    /**
+     * Create modbus packet with various length
+     * @private
+     * @param {number} slave
+     * @param {number} func
+     * @param {number} start
+     * @param {number[]} array
+     * @returns {Buffer}
+     */
+    createVariousPacket(slave, func, start, array) {
+        const buf = (new BufferPut())
+            .word8be(slave)
+            .word8be(func)
+            .word16be(start)
+            .word16be(array.length)
+            .word8be(array.length * 2);
+
+        array.forEach((value) => buf.word16be(value));
+
+        return buf.buffer();
+    }
+
+    /**
+     * @private
+     * @param {Buffer} buffer
+     * @returns {Promise<Buffer>}
+     */
+    request(buffer) {
+        return this.serial.write(packetUtils.addCrc(buffer))
+            .then((response) => {
+                if (!packetUtils.checkCrc(response)) {
+                    throw new errors.ModbusCrcError();
+                }
+                return response;
+            });
+    }
+
+    /**
+     * @private
+     * @param string
+     */
+    log(string) {
+        if (this._options.debug) {
+            console.log(string);
+        }
+    }
+}
